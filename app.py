8f715c9d0a453976c975c41c4ea258d7bcc321e1
Ivan777Vidal-spec <Ivan777Vidal@gmail.com>
2026-08-21 11:15:04 -0700

    Load turnover input update without changing print pipeline

--- app.py
+++ app.py
@@ -30,6 +30,7 @@ def turnovers():
         '<script src="/static/turnover-enhancements.js"></script>'
         '<script src="/static/turnover-mobile-fixes.js"></script>'
         '<script src="/static/turnover-crew-work-order.js"></script>'
+        '<script src="/static/turnover-input-updates.js"></script>'
         '<script src="/static/turnover-pricing-fixes.js"></script>'
         '<script src="/static/turnover-scope-print-update.js"></script>'
         '<script src="/static/turnover-targeted-print-balance.js"></script>'
